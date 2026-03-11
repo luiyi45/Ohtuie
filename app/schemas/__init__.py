0dd28cbdd9ddb93f5c46a03579492551119309d3
luiyi45 <luiyi4455@gmail.com>
2026-03-11 13:20:30 -0500

    Se arregla el archivo que inicia Ãla base de datos

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .token import Token, TokenPayload
-from .user import User, UserCreate, UserInDB, UserUpdate, UserRegistration
+from .user import User, UserCreate, UserInDB, UserUpdate, UserRegistration, UserUpdatePassword
 from .cycle import Cycle, CycleCreate, CycleUpdate
 from .daily_log import DailyLog, DailyLogCreate, DailyLogUpdate
 from .msg import Msg
