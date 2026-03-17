6b71826d5193cbb42138d7ad9c6c3c60fea1d9f3
luiyi45 <luiyi4455@gmail.com>
2026-03-16 19:08:41 -0500

    Se arregla crash del back

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate, UserRegistration, UserUpdatePassword
-from .cycle import Cycle, CycleCreate, CycleUpdate
+from .cycle import Cycle, CycleCreate, CycleUpdate, DeleteBatchRequest
 from .daily_log import DailyLog, DailyLogCreate, DailyLogUpdate
 from .msg import Msg
 from .admin import AdminStatistics
