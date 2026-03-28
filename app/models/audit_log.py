b94b36a5eb7c61f29f4fc1884f12fd9f40947e59
SararRomero <hsromerosara@gmail.com>
2026-03-27 22:59:26 -0500

    Se corrigen errores de formato que se enviaba a la base de datos

--- app/models/audit_log.py
+++ app/models/audit_log.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, String, DateTime, JSON
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.sql import func
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import UUID, JSONB
 import uuid
 
 from app.db.base import Base
@@ -11,5 +11,5 @@ class AuditLog(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     event_type = Column(String, nullable=False) # e.g., "failed_login", "user_registration", "system_update"
     description = Column(String, nullable=True)
-    metadata_json = Column(JSON, nullable=True) # Extra data like IP, user agent, etc.
+    metadata_json = Column(JSONB, nullable=True) # Extra data like IP, user agent, etc.
     created_at = Column(DateTime(timezone=True), server_default=func.now())
